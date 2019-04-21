04a6a20f301e9086c0e536fb2717f5f61f7414f4
raulcuth <raul.cuth@gmail.com>
2019-04-21 16:26:24 +0300

    added missing files

---
+++ Assets/Scripts/MTank/MTank.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MTank : MonoBehaviour
+{
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();    
+    }
+
+    private void Move()
+    {
+        Vector3 movement = transform.forward * speed * Time.deltaTime;
+        transform.position += movement;
+    }
+}
